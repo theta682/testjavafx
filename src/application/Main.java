082417f85676d295471f1d003994e0d0b469f1c8
Timothy Lyanguzov <theta682@gmail.com>
2018-02-24 23:08:58 +1300

    Update layout

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,11 +10,10 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			primaryStage.setTitle("My JavaFX Application");
 			Parent root = (Parent)FXMLLoader.load(getClass().getResource("Main.fxml"));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
-			primaryStage.setTitle("My JavaFX Application");
 			primaryStage.show();
 		} catch (Exception e) {
 			e.printStackTrace();
