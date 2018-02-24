f8f2a50a885b251e7c7814256c3cac6a6b9c929b
Timothy Lyanguzov <theta682@gmail.com>
2018-02-24 19:18:20 +1300

    Add action

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,14 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
 public class MainController {
-	
+	@FXML
+	private Text fileNewTarget;
+
+	@FXML
+	protected void handleMenuFileNew() {
+		fileNewTarget.setText("New was activated");
+	}
 }
