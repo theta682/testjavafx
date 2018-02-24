166142e3ad4042ac89c9afe343b07d15c367cce2
Timothy Lyanguzov <theta682@gmail.com>
2018-02-24 19:56:43 +1300

    Make full controller

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,14 +1,28 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 public class MainController {
 	@FXML
+	private URL location;
+
+	@FXML
+	private ResourceBundle resources;
+
+	@FXML
 	private Text fileNewTarget;
 
 	@FXML
-	protected void handleMenuFileNew() {
+	protected void handleMenuFileNew(ActionEvent event) {
 		fileNewTarget.setText("New was activated");
+	}
+
+	@FXML
+	void initialize() {
+		assert fileNewTarget != null : "fx:id=\"fileNewTarget\" was not injected: check your FXML file 'Main.fxml'.";
 	}
 }
