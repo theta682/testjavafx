95ebae2ae8c908c3d2d4863c30e23fa9a423a55f
Timothy Lyanguzov <theta682@gmail.com>
2018-02-24 18:44:35 +1300

    Use fxml generated by SceneBuilder

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,16 +2,16 @@ package application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root, 800, 600);
+			Parent root = (Parent)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("My JavaFX Application");
