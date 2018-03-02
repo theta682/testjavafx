db01436ab0206c90caed0130ddab6f801fa1bf92
Timothy Lyanguzov <theta682@gmail.com>
2018-03-02 15:51:15 +1300

    Add ComboBox

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -4,9 +4,30 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.text.Text;
 
-public class MainController {
+public class MainController implements Initializable {
+
+	public enum EntityType {
+		NA("Not Applicable"),
+		Type1("Type 1");
+
+		private final String name;
+		private EntityType(String s) {
+			name = s;
+		}
+
+		public boolean equalsName(String otherName) {
+			return name.equals(otherName);
+		}
+
+		public String toString() {
+			return name;
+		}
+	}
+
 	@FXML
 	private URL location;
 
@@ -17,12 +38,20 @@ public class MainController {
 	private Text fileNewTarget;
 
 	@FXML
+	private ComboBox<EntityType> comboType;
+	public EntityType type;
+
+	@FXML
 	protected void handleMenuFileNew(ActionEvent event) {
 		fileNewTarget.setText("New was activated");
 	}
 
-	@FXML
-	void initialize() {
-		assert fileNewTarget != null : "fx:id=\"fileNewTarget\" was not injected: check your FXML file 'Main.fxml'.";
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		type = EntityType.NA;
+		comboType.getItems().addAll(
+			EntityType.NA,
+			EntityType.Type1);
+		comboType.getSelectionModel().select(EntityType.NA);
 	}
 }
